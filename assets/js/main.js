03398f8b80e71b83a19852c56b9a40ead70639fe
MirkOrlando <mirko@orlando-m.it>
2022-03-25 17:21:18 +0100

    created #return button

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,14 +14,17 @@ per indicare centesimi sul prezzo). */
 const unit_price = 0.21
 const underage_discount = 0.2
 const over65_discount = 0.4
-const elementBtn = document.getElementById('submit')
-
+const elementBtnSubmit = document.getElementById('submit')
+const elementBtnReturn = document.getElementById('return')
 
 
 /* estrarre i dati scritti dall'utente nel form */
 
-elementBtn.addEventListener('click', function(){
+elementBtnSubmit.addEventListener('click', function(){
     const fullName = document.getElementById('name').value;
+    if (fullName.length < 1 || fullName.length > 25) {
+        alert('In "nome cognome" puoi inserire da un minimo di 1 carattere fino a un massimo di 25 carattere')
+    }
     const tripLengthKm = document.getElementById('km').value;
     const passengerAge = document.getElementById('age').value;
     console.log(fullName, tripLengthKm, passengerAge)
@@ -37,10 +40,12 @@ elementBtn.addEventListener('click', function(){
     console.log(standardPrice, underPrice, over65Price)
     const wagonNumber = Math.floor(Math.random()*20+1)
     const cp = Math.floor(Math.random()*99998+1)
+    const ticket = document.querySelector('.container_ticket').classList.remove('invisible')
 
     /* visualizzare il risultato */
     document.getElementById('wagon').innerHTML = wagonNumber;
     document.getElementById('code_cp').innerHTML = cp;
+
 
     if (passengerAge === 'under') {
         document.getElementById('ticket_type').innerHTML = 'Sconto minorenne';
@@ -51,13 +56,15 @@ elementBtn.addEventListener('click', function(){
     } else {
         document.getElementById('ticket_type').innerHTML = 'biglietto standard';
         document.getElementById('final_price').innerHTML = `${standardPrice.toFixed(2)} €`;
-    };
+    };    
 });
 
+elementBtnReturn.addEventListener('click', function(){
+    const fullName = document.getElementById('name').value = '';
+    const tripLengthKm = document.getElementById('km').value = '';
+    const passengerAge = document.getElementById('age').value = '';
 
+    console.log(standardPrice, underPrice, over65Price)
 
-
-
-
-
-
+    const ticket = document.querySelector('.container_ticket').classList.add('invisible');
+});
